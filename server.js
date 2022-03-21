cac63057ef27ef25d3cf15d3c5c5ceec22eec6cb
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-03-20 22:50:14 -0400

    final test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 const apiRoute = require("./routes/apiRoute");
 const htmlRoute = require("./routes/htmlRoute");
